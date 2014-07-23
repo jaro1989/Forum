0658d6340d1e3cd8d9e2bc5796a80072c02b4571
Vitaly <vmentuz89@gmail.com>
2014-07-23 21:28:56 +0300

    add_post small fix

--- view/forum_in.php
+++ view/forum_in.php
@@ -1,12 +1,11 @@
-
 <div class="container" >
     <div class="row "> 
         <div class="col-md-3 top-margin">  
         </div>
-        <!--Categories and other -->            
-        <div class="col-md-4 top-margin">  
-            <?php if (isset($_SESSION['userName'])) { ?>
-                <form class="form-control-static" action="index.php?page=forum_in&&action=done&&cat_id=<?= $_GET['cat_id']; ?>" method="post">
+        <!--Categories and other -->
+        <?php if (isset($_SESSION['userName'])) { ?>
+            <div class="col-md-4 top-margin">   
+                <form class="form-control-static" action="index.php?page=forum_in&&action=done&&cat_id=<? echo $_GET['cat_id'];?>"method="post">
                     <fieldset>
 
                         <!-- Form Name -->
@@ -15,7 +14,7 @@
                         <!-- Text input-->
                         <div class="control-group">
                             <div>       
-                                <label class="control-label" for="email">Заголовок сообщения:</label>
+                                <label class="control-label" for="title">Заголовок сообщения:</label>
                             </div>
                             <div>
                                 <input id="post_title" name="post_title" placeholder="" class="input-medium" required="" type="text">
@@ -26,16 +25,22 @@
                             <div>
                                 <textarea id="about" cols="50" name="post_text"></textarea>
                             </div>
+                            <div>       
+                                <label class="control-label" for="tags">Тэги:</label>
+                            </div>
+                            <div>
+                                <input id="tags" name="tags" placeholder="" class="input-medium" required="" type="text">
+                                <p class="help-block">Введите тэги через запятую (Пример: работа, бизнес)</p>
+                            </div>
                             <div>
                                 <button type="submit" name="Submit" class="btn btn-primary">Создать запись</button>
                             </div>
                         </div>
-
-
+                        </div>
+                        </div>
 
                     </fieldset>
                 </form>
-
             <?php } else { ?>
                 <h2>Зарегистрируйтесь, чтобы добавить запись</h2>
             <?php } ?>
@@ -43,37 +48,41 @@
     </div>
 </div>
 <!--End jumbotron-->
+<div class="container">
+    <!--Blog Start-->  
+    <div class="row "> 
+        <!--Categories and other -->   
 
-<!--Blog Start-->  
-<div class="row "> 
-    <!--Categories and other -->   
-    <div class="col-md-2 top-margin">
-    </div>         
-    <div class="col-md-4 top-margin">
+        <div class="col-md-4 top-margin">
 
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                Список категорий
-            </div>
-            <div class="panel-body">
-                <ul>
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    Список категорий
+                </div>
+                <div class="panel-body">
+
                     <?php echo ContentManager::renderCategories($categories, "messNum"); ?>
+
+                </div>
+
+            </div>
+
+        </div>
+        <!--End Categories and other --> 
+        <!--Blog Listing--> 
+        <div class="col-md-8 top-margin" >
+            <div >
+                <ul class="timeline">
+                    <?php echo ContentManager::renderPost($posts); ?>
                 </ul>
             </div>
-
         </div>
+        <!--Blog Listing--> 
 
     </div>
-    <!--End Categories and other --> 
-    <!--Blog Listing--> 
-    <div class="col-md-6 top-margin" >
-        <div >
-            <ul class="timeline">
-                <?php echo ContentManager::renderPost($posts); ?>
-            </ul>
-        </div>
-    </div>
-    <!--Blog Listing--> 
+</div>
+<!--Blog end-->  
+
 
 </div>
-<!--Blog end-->
+<!-- /.container -->
